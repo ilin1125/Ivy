3fb757c2d02c7f80dd91f46ee4b82c5ece722889
ilin1125 <ilin1125@gmail.com>
2025-10-21 16:26:04 +0800

    Add service worker registration in index.js
    Register service worker for the application.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,4 +8,9 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+); if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    // 你的網站會在 /ivy/ 底下，所以用相對路徑最安全
+    navigator.serviceWorker.register('./service-worker.js').catch(() => {});
+  });
+}
